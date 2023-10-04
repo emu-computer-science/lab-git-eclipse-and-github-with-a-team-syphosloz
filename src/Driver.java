00128d7ccfb8745bab471c1f8203dc7a8dafffaf
Joshua Randall <jranda10@emich.edu>
2023-10-04 18:35:14 -0400

    First class call

--- src/Driver.java
+++ src/Driver.java
@@ -2,8 +2,9 @@
 public class Driver {
 	
 	public static void main(String[] args) {
-		
-	
 	System.out.println("Our team is getting started.");
+	
+//	First class call
+	Random.print();
 	}
 }
---
+++ src/Random.java
@@ -0,0 +1,6 @@
+
+public class Random {
+	public static void print() {
+		System.out.println("This is the first new class in the repo.");
+	}
+}
